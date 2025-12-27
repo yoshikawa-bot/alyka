252702ff9ac569d793ff4ab241cc8a274c95e0a2
Yoshikawa Project <ilogs.lya@gmail.com>
2025-12-27 14:07:11 -0300

    Update tiktok.js

--- api/tiktok.js
+++ api/tiktok.js
@@ -1,5 +1,5 @@
 const { fbdl } = require('ruhend-scraper');
-
+const cheerio = require('cheerio'); 
 module.exports = async (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     const { url } = req.query;
