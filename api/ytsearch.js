dfd60840198bbabb8c056e48afc0b99062d4d583
Yoshikawa Project <ilogs.lya@gmail.com>
2025-12-27 02:16:57 -0300

    Update ytsearch.js

--- api/ytsearch.js
+++ api/ytsearch.js
@@ -1,14 +1,13 @@
-const { ytsearch } = require('ruhend-scraper');
+import { ytsearch } from 'ruhend-scraper';
 
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    const { query } = req.query; 
+    const { query } = req.query;
 
     if (!query) return res.status(400).json({ status: false, message: "Envie o parâmetro ?query=" });
 
     try {
         const { video, channel } = await ytsearch(query);
-
         return res.status(200).json({ 
             status: true, 
             creator: "Alyka", 
@@ -20,4 +19,4 @@ module.exports = async (req, res) => {
     } catch (error) {
         return res.status(500).json({ status: false, message: error.message });
     }
-};
+}
